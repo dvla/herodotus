e86e5dd9e9498474cc276bb41c2bdbbf1bc044d8
nialljames <niall.james@dvla.gov.uk>
2025-10-06 07:47:39 +0100

    refactor

--- lib/dvla/herodotus/herodotus_logger.rb
+++ lib/dvla/herodotus/herodotus_logger.rb
@@ -16,6 +16,7 @@ module DVLA
         @main = config[:main]
         @display_pid = config[:display_pid]
         @prefix_colour = config[:prefix_colour]
+        @colour_methods = build_colour_methods(@prefix_colour)
 
         @correlation_id = SecureRandom.uuid[0, 8]
         set_formatter
@@ -70,48 +71,79 @@ module DVLA
       def set_formatter
         self.formatter = proc do |severity, _datetime, _progname, msg|
           now = Time.now
-          system = @system_name
-          date = now.strftime('%Y-%m-%d')
-          time = now.strftime('%H:%M:%S')
-          correlation = @correlation_id
-          pid = @display_pid ? Process.pid.to_s : nil
-          level = severity
-          separator = '-- :'
+          components = {
+            system: @system_name,
+            date: now.strftime('%Y-%m-%d'),
+            time: now.strftime('%H:%M:%S'),
+            correlation: @correlation_id,
+            pid: @display_pid ? Process.pid.to_s : nil,
+            level: severity,
+            separator: '-- :',
+          }
 
-          prefix = case @prefix_colour
-                     # Colourise the whole prefix
-                   when Array, String
-                     bracket_content = [system, date, time, correlation, pid].compact.join(' ')
-                     colourise_text("[#{bracket_content}] #{level} #{separator} ", @prefix_colour)
-                   when Hash
-                     #   Colour each component individually and wrap in an overall colour
-                     s = @prefix_colour[:system] ? colourise_text(system, @prefix_colour[:system]) : system
-                     d = @prefix_colour[:date] ? colourise_text(date, @prefix_colour[:date]) : date
-                     t = @prefix_colour[:time] ? colourise_text(time, @prefix_colour[:time]) : time
-                     c = @prefix_colour[:correlation] ? colourise_text(correlation, @prefix_colour[:correlation]) : correlation
-                     p = pid && @prefix_colour[:pid] ? colourise_text(pid, @prefix_colour[:pid]) : pid
-                     l = @prefix_colour[:level] ? colourise_text(level, @prefix_colour[:level]) : level
-                     sep = @prefix_colour[:separator] ? colourise_text(separator, @prefix_colour[:separator]) : separator
-                     bracket_content = [s, d, t, c, p].compact.join(' ')
-                     result = "[#{bracket_content}] #{l} #{sep} "
-                     @prefix_colour[:overall] ? colourise_text(result, @prefix_colour[:overall]) : result
-                   else
-                     # No colourisation
-                     bracket_content = [system, date, time, correlation, pid].compact.join(' ')
-                     "[#{bracket_content}] #{level} #{separator} "
-                   end
-
+          prefix = apply_prefix_colors(components)
           "#{prefix}#{msg}\n"
         end
       end
 
     private
 
-      def colourise_text(text, colour_spec)
-        return text unless colour_spec
+      VALID_METHODS = %w[
+        white black red green brown yellow blue magenta cyan gray grey
+        bright_red bright_green bright_blue bright_magenta bright_cyan
+        bg_black bg_red bg_green bg_brown bg_yellow bg_blue bg_magenta bg_cyan bg_gray bg_grey bg_white
+        bg_bright_red bg_bright_green bg_bright_blue bg_bright_magenta bg_bright_cyan
+        bold dim italic underline reverse_colour reverse_color
+      ].freeze
 
-        methods = colour_spec.is_a?(Array) ? colour_spec : colour_spec.to_s.split('.')
-        methods.reduce(text) { |str, method| str.public_send(method) }
+      def build_colour_methods(colour_spec)
+        case colour_spec
+        when Array, String
+          methods = colour_spec.is_a?(Array) ? colour_spec : colour_spec.split('.')
+          return {} unless methods.all? { |m| VALID_METHODS.include?(m) }
+
+          { prefix: methods.map(&:to_sym) }
+        when Hash
+          colour_spec.transform_values do |spec|
+            next unless spec
+
+            methods = spec.is_a?(Array) ? spec : spec.split('.')
+            methods.map(&:to_sym) if methods.all? { |m| VALID_METHODS.include?(m) }
+          end.compact
+        else
+          {}
+        end
+      end
+
+      def apply_colors(text, color_methods)
+        return text unless color_methods
+
+        color_methods.reduce(text) { |str, method| str.public_send(method) }
+      end
+
+      def apply_prefix_colors(components)
+        if @colour_methods[:prefix]
+          bracket_content = [components[:system], components[:date], components[:time], 
+                             components[:correlation], components[:pid]].compact.join(' ')
+          text = "[#{bracket_content}] #{components[:level]} #{components[:separator]} "
+          apply_colors(text, @colour_methods[:prefix])
+        elsif @colour_methods.is_a?(Hash) && @colour_methods.any?
+          system = apply_colors(components[:system], @colour_methods[:system])
+          date = apply_colors(components[:date], @colour_methods[:date])
+          time = apply_colors(components[:time], @colour_methods[:time])
+          correlation = apply_colors(components[:correlation], @colour_methods[:correlation])
+          pid = components[:pid] && apply_colors(components[:pid], @colour_methods[:pid])
+          level = apply_colors(components[:level], @colour_methods[:level])
+          separator = apply_colors(components[:separator], @colour_methods[:separator])
+
+          bracket_content = [system, date, time, correlation, pid].compact.join(' ')
+          result = "[#{bracket_content}] #{level} #{separator} "
+          apply_colors(result, @colour_methods[:overall]) || result
+        else
+          bracket_content = [components[:system], components[:date], components[:time], 
+                             components[:correlation], components[:pid]].compact.join(' ')
+          "[#{bracket_content}] #{components[:level]} #{components[:separator]} "
+        end
       end
 
       def set_proc_writer_scenario
